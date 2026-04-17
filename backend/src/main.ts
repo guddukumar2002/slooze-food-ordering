db1814bccf2ea724db20bd7b342ec6fef6d5659c
Guddu <gk13212@gmail.com>
2026-04-17 14:12:50 +0530

    fix: add Vercel URLs to CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://slooze-food-ordering.vercel.app',
+      'https://slooze-food-ordering-git-main-guddu-kumars-projects-9014a0ff.vercel.app',
+    ],
+    credentials: true,
+  });
   await app.listen(4000);
   console.log('Backend running on http://localhost:4000/graphql');
 }
